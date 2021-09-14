fa77e08134a998a67c3dbe6e55e692ddf4b7d17b
Gustavo Priebe <gustavo_priebe@hotmail.com>
2021-09-14 18:58:52 -0300

    finished quizz app

--- quizz-app/assets/js/script.js
+++ quizz-app/assets/js/script.js
@@ -1,3 +1,4 @@
+// BD de perguntas
 const quizData = [
     {
         question:
@@ -46,16 +47,24 @@ const quizData = [
     },
 ];
 
-const question = document.querySelector('.question');
-const answerA = document.querySelector('.answerA');
-const answerB = document.querySelector('.answerB');
-const answerC = document.querySelector('.answerC');
-const answerD = document.querySelector('.answerD');
+// DOM
+const question = document.querySelector(".question");
+const answerA = document.querySelector(".answerA");
+const answerB = document.querySelector(".answerB");
+const answerC = document.querySelector(".answerC");
+const answerD = document.querySelector(".answerD");
+const button = document.querySelector(".button");
+const checked = document.querySelectorAll(".answer");
+const quiz = document.querySelector(".perguntas");
 
-
+// Variáveis
+let score = 0;
 let currentQuestion = 0;
 
-function quizApp () {
+// Jogando as perguntas no HTML
+function quizApp() {
+    deselectAnswer();
+
     const questionData = quizData[currentQuestion];
 
     question.innerText = questionData.question;
@@ -63,21 +72,46 @@ function quizApp () {
     answerB.innerText = questionData.b;
     answerC.innerText = questionData.c;
     answerD.innerText = questionData.d;
-    
-
-
-    currentQuestion++;
-};
-
-
-function quizResult () {
-    
 }
 
+//Selecionar resposta do questionário
+function selectedAnswer() {
+    let answer = undefined;
 
-addEventListener('click', quizApp);
+    checked.forEach((answerEl) => {
+        if (answerEl.checked) answer = answerEl.id;
+    });
 
+    return answer;
+}
 
+// Limpar seleção de resposta
+function deselectAnswer() {
+    checked.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+// Lógica do Quiz
+function quizResult() {
+
+    console.log("Done");
+
+    quiz.innerHTML = `<h2 style="text-align: center; margin-top: 100px;">O número de acertos foi:</h2><h1 style="text-align: center"> ${score}/${quizData.length}</h1>`;
+
+    button.innerHTML = `<h3 onclick="location.reload()">Tentar novamente</h3>`;
+}
+
+// Funcionalidade do botão Enviar
+button.addEventListener("click", () => {
+    const answer = selectedAnswer();
+
+    if (answer === quizData[currentQuestion].correct) score++;
+    currentQuestion++;
+
+    console.log(score);
+
+    currentQuestion < quizData.length ? quizApp() : quizResult();
+});
 
 quizApp();
-
